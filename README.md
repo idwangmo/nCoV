0a37b83e6a1086fb9ee3c0781c0ba16889c34b6a
idwangmo <idwangmo@protonmail.com>
2020-01-24 19:06:36 +0800

    docs: ✏️ 增加文档

---
+++ README.md
@@ -0,0 +1,5 @@
+# nCoV
+
+## 参考
+
+春节闲的照着方糖的[nCoV-push](https://github.com/easychen/nCoV-push)防止的一个项目，没啥核心逻辑，就是无聊
---
+++ ncov.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# 这里我使用的是 conda 进行包管理，可以进行适当的修改
+export PATH="/home/idwangmo/.minicoda3/bin:$PATH"
+
+eval "$(conda shell.bash hook)"
+
+conda activate nCov
+
+cd /home/idwangmo/nCoV || exit
+
+scrapy crawl ncov
